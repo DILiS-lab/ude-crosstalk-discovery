4474156920587f9dc806b59f56065b7f27ac3ece
umurcankaya <umurcan.kaya@gmail.com>
2026-03-16 14:28:01 +0100

    Cleanup: Removed unused figures and scripts.

--- code/plots/paper_plots/synth_big_fig.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-fig = plt.figure(figsize=(7.00787, 2.5))
-ax_K_crosstalk = fig.add_subplot(2, 3, 1)
-ax_H_crosstalk = fig.add_subplot(2, 3, 2)
-ax_K_box = fig.add_subplot(2, 3, 3)
-ax_H_box = fig.add_subplot(2, 3, 6)
-
-plt.show()
